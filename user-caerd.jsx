787c92152a15155502b134304c67767fb0bfb34c
Jerrin Joy <45188083+jerrinjoy18@users.noreply.github.com>
2021-11-30 14:35:31 +0530

    Add files via upload

---
+++ user-caerd.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./UserCard.css";
+
+function UserCard(props) {
+  return (
+    <div className="User-card" id={props.id}>
+      <div className="Card-details">
+        <img src={props.avatar} alt="avatar-img" />
+        <h3>{`${props.first_name} ${props.last_name}`}</h3>
+        <p>{props.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
